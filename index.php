a0386e37b6308a38d7a19707edcd868bf7567d71
Luckieuh <lucas2.thom@gmail.com>
2025-10-23 18:49:20 +0200

    ...

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="./favicon.ico">
+  <link rel="stylesheet" href="./global.css">
+  <title>MM(I)emes</title>
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <img src="./images/logo.png" alt="Logo">
+    </div>
+    <nav>
+      <div class="burger-menu">
+      <button class="burger">
+        <img src="./images/burger-menu.svg" alt="">
+      </button>
+        <ul class="nav-links">
+          <li><a href="./index.html">Accueil</a></li>
+          <li><a href="./les-memes.html">Les mèmes</a></li>
+          <li><a href="./les-auteurs.html">Les auteurs</a></li>
+          <li><a href="./contact.html">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <h1>MM(I)emes</h1>
+</body>
+</html>
