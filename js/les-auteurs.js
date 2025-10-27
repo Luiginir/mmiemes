17038ab0b277f11f4a0746ed25818087855ce43f
Amir <amir.metmati@gmail.com>
2025-10-27 10:57:52 +0100

    page auteurs + css

--- js/les-auteurs.js
+++ js/les-auteurs.js
@@ -1,4 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const img = document.querySelector('.auteur-image');
+    const img = document.querySelector('.profile-imgs');
+    const infos = document.querySelector('.infos');
 
+    img.addEventListener("click", () => {
+        if (infos[1].className.includes('hidden')) {
+            infos[1].classList.remove('hidden');
+            infos[0].classList.add('hidden');
+        }
+        else {
+            infos[0].classList.remove('hidden');
+            infos[1].classList.add('hidden');
+        }
+    })
 })
--- les-auteurs.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const img = document.querySelector('.auteur-image');
-
-})
