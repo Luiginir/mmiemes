6cff50ee9d600e02ef4d3f621429c9fb94ea6982
Luckieuh <lucas2.thom@gmail.com>
2025-10-27 11:39:35 +0100

    css page index

--- index.php
+++ index.php
@@ -10,6 +10,24 @@
 
   <div class="meme-container">
     <img src="./images/test.png" alt="test image">
+    <div>
+      <i class="fa-solid fa-xmark"></i>
+      
+    </div>
+    <div>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+    </div>
+    <div>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </div>
+  <div>
+    <h2>Nom du meme</h2>
+    <p>Description du meme</p>
   </div>
 </body>
 </html>
