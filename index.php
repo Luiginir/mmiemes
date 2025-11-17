52b1450e1b9d697d86376959cbf5cb752c5877cb
Luckieuh <lucas2.thom@gmail.com>
2025-11-17 19:04:54 +0100

    page accueil

--- index.php
+++ index.php
@@ -6,20 +6,23 @@
 <html lang="fr">
     <?php echo generateHeader($csslink = "./css/index.css") ?>
 <body>
-  <div class="card-container">
-    <div class="tinder--card">
-      <img src="/images/meme1.png">
-      <div class="rating">
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-      </div>
-      <h3>Demo card 1</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div>
+  <div class='title-container'>
+    <img src="./images/largelogo.svg" alt="">
+
+    <h1>Le dernier meme</h1>
   </div>
 
+  <div class="tinder--card">
+      <img src="/images/meme2.png">
+    <div class="rating">
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+    </div>
+    <h3>Demo card 2</h3>
+    <p>This is a demo for Tinder like swipe cards</p>
+  </div>
 </body>
 <script src="./js/header.js"></script>
 </html>
