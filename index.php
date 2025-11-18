26619b303318c2d0be343ae91c22b78ac7e36537
Amir <amir.metmati@gmail.com>
2025-11-18 23:21:27 +0100

    jsp

--- index.php
+++ index.php
@@ -6,23 +6,16 @@
 <html lang="fr">
     <?php echo generateHeader($csslink = "./css/index.css") ?>
 <body>
-  <div class='title-container'>
-    <img src="./images/largelogo.svg" alt="">
-
-    <h1>Le dernier meme</h1>
-  </div>
-
-  <div class="tinder--card">
-      <img src="/images/meme2.png">
-    <div class="rating">
-      <i class="fa-solid fa-star"></i>
-      <i class="fa-solid fa-star"></i>
-      <i class="fa-solid fa-star"></i>
-      <i class="fa-solid fa-star"></i>
+    <div class="tinder--card">
+      <img src="/images/meme1.png">
+      <div class="rating">
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+      </div>
+      <h3>Demo card 1</h3>
+      <p>This is a demo for Tinder like swipe cards</p>
     </div>
-    <h3>Demo card 2</h3>
-    <p>This is a demo for Tinder like swipe cards</p>
-  </div>
 </body>
-<script src="./js/header.js"></script>
 </html>
